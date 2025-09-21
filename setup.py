ac67021fbeaed54f6ebce1b5ecad5f1b95cefa45
Jawad Iqbal <Jawad.Iqbal@SomersetFT.nhs.uk>
2025-09-21 22:30:57 +0100

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jawad_calculator',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     description='A simple calculator with addition and subtraction',
     author='Jawad Iqbal',
